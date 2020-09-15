a6dc2b8562fc1ea07d7e3f9b99db5f83765de97e
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2020-09-15 08:01:47 -0300

    created the connection with github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoGithubExplorer from '../../assets/logo-github-explorer.svg';
+
+import { Title, Form, Repositories } from './styles';
+import Repository from '../Repository';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
-}
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
+  return (
+    <>
+      <img src={logoGithubExplorer} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(event) => setNewRepo(event.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        {repositories.map((repo) => (
+          <a key={repo.full_name} href="test">
+            <img
+              src={repo.owner.avatar_url}
+              alt={repo.owner.login}
+            />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+
+            <FiChevronRight size={30} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
